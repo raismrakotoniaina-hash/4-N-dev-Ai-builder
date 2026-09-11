c2e45b3a60cbd325080a532b3242fc8d6cf1190f
raismrakotoniaina-hash <raismrakotoniaina@gmail.com>
2026-09-11 17:27:10 +0300

    Create App component for loading screen

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+function App() {
+  return (
+    <main className="app-loading">
+      <div className="app-loading__glow" />
+
+      <section className="app-loading__content">
+        <div className="app-loading__logo">
+          <span className="app-loading__logo-number">4</span>
+          <span className="app-loading__logo-text">N</span>
+        </div>
+
+        <p className="app-loading__eyebrow">AI POWERED DEVELOPMENT</p>
+
+        <h1>
+          4-N-DEV
+          <span>AI BUILDER</span>
+        </h1>
+
+        <p className="app-loading__description">
+          Manomana ny intelligent development workspace...
+        </p>
+
+        <div className="app-loading__line">
+          <span />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
